162b3a705cbc2de868f775a1171d202b025fc6b7
Tsogoo <ts.r.madrid@gmail.com>
2012-05-08 17:28:55 +0800

    commit

--- static/js/app_index.js
+++ static/js/app_index.js
@@ -1,6 +1,6 @@
 var News = Backbone.Model.extend({
 })
-var NewsView = Backbone.View.extend({
+var NewsView = CMS.View.extend({
     params:{
         search:'',
         publish:'all',
@@ -10,14 +10,16 @@ var NewsView = Backbone.View.extend({
     edit_fields:[],
     templates:{ list:null, edit:null},
     initialize:function(options){
-        this.model = new News
-        this.route = options.route
         var me = this
-        this.route.route("news/list", "run", function(){me.run({action:'list'})})
-        this.route.route("news/list/:page", "run", function(page){me.run({action:'list',url_params:{page:page}})})
-        this.route.route("news/list/:search/:publish/:page", "run", function(search,publish,page){ me.run({action:'list', url_params:{search:search,publish:publish,page:page}})})
-        this.route.route("news/new", "run", function(){me.run({action:'edit'})})
-        this.route.route("news/edit/:id", "run", function(id){me.run({action:'edit', url_params:{id:id}})})
+        this.model = new News
+        this.alias = 'news'
+        this.router = options.router
+        this.root_url = options.root_url
+        this.router.route("news/list", "run", function(){me.run({action:'list'})})
+        this.router.route("news/list/:page", "run", function(page){me.run({action:'list',url_params:{page:page}})})
+        this.router.route("news/list/:search/:publish/:page", "run", function(search,publish,page){ me.run({action:'list', url_params:{search:search,publish:publish,page:page}})})
+        this.router.route("news/new", "run", function(){me.run({action:'edit'})})
+        this.router.route("news/edit/:id", "run", function(id){me.run({action:'edit', url_params:{id:id}})})
     },
 
 
@@ -27,51 +29,78 @@ var NewsView = Backbone.View.extend({
     run:function(options){
         var me = this
         if(!options.template)
-            options.template = options.action
-        this.init_template({name:options.template, success:function(){
+            options.template = this.alias
+        this.init_template({file_name:options.template, success:function(){
             me.init_model({ action:options.action, params:options.url_params, success:function(){
                 me.render({template:options.template})
             }})
         }, unsuccess:function(){ alert('error')}})
     },
     init_template:function(options){
-        for( temp in this.templates){
-            if(options.name == temp){
-                if(this.templates[temp]){
-                    options.success()
-                }
-                else {
-                    this.templates[temp] = temp
-                    options.success()
-                }
-                return
-            }
+        
+        if(!this.template_string){
+            
+            $.get(this.root_url+'templates/'+options.file_name+'.html',function(data){
+                this.template_string = data
+                $('body').append(this.template_string)
+                options.success()
+            })    
         }
-        options.unsuccess()
+        else{
+            $('body').append(this.template_string)
+            options.success()
+        }
+    },
+    init_model:function(options){
+        this.current_action = options.action
+        options.success()
+    },
+    render:function(options){
+        $('#page_content').empty()
+        $('#page_content').html(_.template($('#template_'+this.alias+'_'+this.current_action).html()))
+    },
+})
+var MainView = CMS.View.extend ({
+    initialize:function(options){
+        this.alias = 'layout'
+        this.root_url = options.root_url
+        this.menus = options.menus
+        this.run({action:'index'})
     },
     init_model:function(options){
         options.success()
     },
     render:function(options){
-        alert(this.templates[options.template])
-    },
+        $('#body').append(_.template($('#template_layout_menu').html(),{menus: this.menus}))
+        $('#body').append('<div id="page_content"></div>')
+    }
 })
 var AppRouter = Backbone.Router.extend({
     modules:{'News':null},
     initialize:function(options){
+        var viewOptions = {router: this, root_url: options.root_url}
+        this.mainMenu = new MainView(viewOptions)
         for(var module in this.modules){
-            var view = eval('new '+module+'View({route:this})')
+        //    var view = eval('new '+module+'View(viewOptions)')
         }
+        
     },
     routes: {
         "": "index",
         "help": "help",
     },
     index: function(page){
-        alert(1)
     }
 })
 $(function(){
-    var appRouter = new AppRouter({});
+    var menus = [
+        {label:'Хянах самбар', url:'',items:[]},
+        {label:'Мэдээ мэдээлэл', url:'news',items:[]},
+        {label:'Мэдиа', url:'fm',items:[
+            {label:'Image manager', url:'im'},
+            {label:'File manager', url:'fm'}
+        ]},
+    ]
+    var appRouter = new AppRouter({root_url:'/static/', menus: menus});
     Backbone.history.start();
 })
---
+++ views.py
@@ -0,0 +1,7 @@
+# Create your views here.
+from django.http import HttpResponse
+from django.template import Context, loader
+def home(request, *args, **kwargs):
+    t = loader.get_template("templates/index.html")
+    return HttpResponse(t.render(Context({'MEDIA_URL':'/static/'})))
+    
